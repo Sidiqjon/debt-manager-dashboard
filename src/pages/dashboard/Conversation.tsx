d8aabd0ab6694ea178ad07dc410bfa8408b6e2a1
Sidiqjon <sidiqjonyusufjanov7@gmail.com>
2026-07-11 06:01:47 +0500

    updated

--- src/pages/dashboard/Conversation.tsx
+++ src/pages/dashboard/Conversation.tsx
@@ -25,7 +25,7 @@ const Conversation = () => {
 
   const FindMonth = (index: number) => {
     const monthList = ["Yanvar", "Fevral", "Mart", "Aprel", "May", "Iyun", "Iyul", "Avgust", "Sentabr", "Oktabr", "Noyabr", "Dekabr"];
-    return monthList[index];
+    return monthList[index - 1];
   }
 
   const { data = [], isLoading } = useQuery({
